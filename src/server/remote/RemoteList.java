10be9d75b91f1c258e4bd237144f82941411a521
clemeno <clemeno@gmail.com>
2013-10-08 12:30:47 +0200

    server update RemoteList and doc 001

---
+++ src/server/remote/RemoteList.java
@@ -0,0 +1,24 @@
+package server.remote;
+
+import java.rmi.server.RemoteObject;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RemoteList<T> extends RemoteObject {
+	
+	private static final long serialVersionUID = -2089731920348238926L;
+	
+	List<T> l; 
+	
+	public RemoteList() {
+		this.l = new LinkedList<T>(); 
+	}
+	
+	public void add( T e ) {
+		this.l.add( e ); 
+	}
+	
+	public int size() {
+		return this.l.size(); 
+	}
+}
