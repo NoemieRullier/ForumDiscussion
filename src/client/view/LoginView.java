5e376a4a935ce48453e013aeab834aebc0ddd01f
Nomyx <r.noemie25@orange.fr>
2013-10-15 21:58:22 +0200

    Finish ihm login

--- src/client/view/LoginView.java
+++ src/client/view/LoginView.java
@@ -22,6 +22,8 @@ public class LoginView extends JFrame {
 	private JPanel panel = new JPanel();
 	private JButton verifyButton = new JButton();
 	private ImageIcon verifyIcon = new ImageIcon("img/recycling_32.png");
+	private JButton validateButton = new JButton("Valider");
+	private ImageIcon validateIcon = new ImageIcon("img/badge_32.png");
 	
 	public LoginView(){
 		panel.setLayout(new GridBagLayout());
@@ -48,6 +50,17 @@ public class LoginView extends JFrame {
 		gbc.insets = new Insets(10, 5, 5, 10);
 		verifyButton.setIcon(verifyIcon);
 		panel.add(verifyButton, gbc);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.gridwidth = GridBagConstraints.REMAINDER;
+		gbc.gridheight = 1;
+		gbc.weightx = 1;
+		gbc.weighty = 0;
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.anchor = GridBagConstraints.CENTER;
+		gbc.insets = new Insets(5, 10, 5, 10);
+		validateButton.setIcon(validateIcon);
+		panel.add(validateButton, gbc);
 		
 		this.setSize(400, 200);
 		this.setLocationRelativeTo(null);
