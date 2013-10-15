53fd58bb784f91d7ebdd3359caf209ac292c2d88
clemeno <clemeno@gmail.com>
2013-10-15 11:49:12 +0200

    ajoute le mainclient please

---
+++ src/client/MainClient.java
@@ -0,0 +1,53 @@
+package client;
+
+import java.net.InetAddress;
+import java.rmi.Naming;
+import java.util.ArrayList;
+import java.util.List;
+
+import client.controller.ClientController;
+import client.controller.IClientController;
+import client.model.Client;
+import client.model.IClient;
+import client.view.MainWindowClient;
+import server.IServerForum;
+import server.ISubjectDiscussion;
+
+public class MainClient {
+	
+	private static List<ISubjectDiscussion> listSubject = new ArrayList<ISubjectDiscussion>();
+	
+	private static IClient client; 
+	private static IClientController controller; 
+	
+
+	public static void main(String[] args) {
+		try {
+			String serverAddress = "//" + InetAddress.getLocalHost().getHostAddress() + ":1099/ServerForum";
+			//String serverAddress = "//192.168.137.1:1099/ServerForum";
+			IServerForum chatServer = (IServerForum) Naming.lookup(serverAddress);
+			System.out.println("Connexion OK");
+			ISubjectDiscussion sujet; 
+			int nbSujets = chatServer.nbSujets(); 
+			System.out.println( nbSujets );
+			int i; 
+			for ( i = 0 ; i < nbSujets ; i++ ) {
+				sujet = chatServer.sendSubject( i ); 
+				System.out.println( sujet.getTitle() ); 
+				listSubject.add( sujet ); 
+			}
+			System.out.println( "Liste locale : nbElem " + listSubject.size() );
+			
+			controller = new ClientController(); 
+			client = new Client( controller ); 
+			
+			
+			MainWindowClient window = new MainWindowClient(listSubject, controller, client);
+		}
+		catch (Exception e){
+			e.printStackTrace();
+			System.out.println("Ca marche pas");
+		}
+	}
+
+}
