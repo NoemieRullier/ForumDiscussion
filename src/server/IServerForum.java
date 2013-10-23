519e8309f0b6e1fccddba40f518d79bcf7fba048
Nomyx <r.noemie25@orange.fr>
2013-10-23 15:41:11 +0200

    Add some javadoc

--- src/server/IServerForum.java
+++ src/server/IServerForum.java
@@ -5,20 +5,51 @@ import java.rmi.RemoteException;
 
 public interface IServerForum extends Remote {
 
-	/*
-	 * TODO A revoir comment on fait la doc (je me souviens plus)
-	 * @result The SubjectDiscussion corresponding to title or null if subject doesn't exist
+	/**
+	 * Get the subject associate at the title
+	 * @param title
+	 * @return The SubjectDiscussion corresponding to title or null if subject doesn't exist
+	 * @throws RemoteException
 	 */
 	public ISubjectDiscussion getSubject(String title) throws RemoteException;
-	
+
+	/**
+	 * Add a new subject on the sever
+	 * @param title
+	 * @return true if the subject doesn't exist and is add, false otherwise
+	 * @throws RemoteException
+	 */
 	public boolean addSubjectDiscussion(String title) throws RemoteException; 
-	
+
+
+	/**
+	 * Get number of subjects save on server 
+	 * @return Number of subjects
+	 * @throws RemoteException
+	 */
 	public int nbSujets() throws RemoteException;
 
+	/**
+	 * Get the subject associate at the position pos
+	 * @param pos
+	 * @return The SubjectDiscussion corresponding to the pos in the list of subjects
+	 * @throws RemoteException
+	 */
 	public ISubjectDiscussion sendSubject( int pos ) throws RemoteException;
-	
+
+	/**
+	 * Define if the pseudo is available
+	 * @param pseudo
+	 * @return true if available, false otherwise
+	 * @throws RemoteException
+	 */
 	public boolean pseudoAvailable(String pseudo) throws RemoteException;
 
+	/**
+	 * Delete the pseudo in list
+	 * @param login
+	 * @throws RemoteException
+	 */
 	public void removeLogin(String login) throws RemoteException;
-	
+
 }
--- src/server/ISubjectDiscussion.java
+++ src/server/ISubjectDiscussion.java
@@ -7,23 +7,35 @@ import client.model.IClient;
 
 public interface ISubjectDiscussion extends Remote {
 
-	/*
-	 * Register client to this subject discussion
+	/**
+	 * Register client c to this subject discussion
+	 * @param c
+	 * @return true if client was register, false otherwise
+	 * @throws RemoteException
 	 */
 	public boolean registration (IClient c) throws RemoteException;
-	
-	
-	/*
+
+	/**
 	 * Broadcast the message msg to all client register
+	 * @param msg
+	 * @throws RemoteException
 	 */
 	public void broadcast(String msg) throws RemoteException;
 
-	/*
-	 * Return the title of the subject discussion
+	/**
+	 * Get the title of this subject discussion
+	 * @return title
+	 * @throws RemoteException
 	 */
 	public String getTitle() throws RemoteException;
 
+	/**
+	 * Unsubscribe the client c
+	 * @param c
+	 * @return true if the client was unsubscribe, false otherwise
+	 * @throws RemoteException
+	 */
 	public boolean unsubscribe(IClient c) throws RemoteException;
-	
-	
+
+
 }
